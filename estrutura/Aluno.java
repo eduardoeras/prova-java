03416c5ff72f1ee1f1200e4b014d3cd982806f6a
Eduardo Rohde Eras <eduardorohdeeras@gmail.com>
2019-12-16 12:46:01 -0300

    Criado o bean "Aluno"

--- estrutura/Aluno.java
+++ estrutura/Aluno.java
@@ -1,8 +1,36 @@
 package estrutura;
 
 class Aluno {
-  private String mId;
-  private String mNome;
-  private float  mMedia;
+  //Escolhi manter o padrão no nome dos atributos com uma palavra simples
+  private String id;
+  private String nome;
+  private float  media;
+
+  //Setters
+  public void setId (String id) {
+    this.id = id;
+  }
+
+  public void setNome (String nome) {
+    this.nome = nome;
+  }
+
+  public void setMedia (float media) {
+    this.media = media;
+  }
+
+  //Getters
+  public String getId () {
+    return id;
+  }
+
+  public String getNome () {
+    return nome;
+  }
+
+  public float getMedia () {
+    return media;
+  }
+
 }
 
--- logica/LeitorGravador.java
+++ logica/LeitorGravador.java
@@ -6,6 +6,7 @@ public class LeitorGravador {
     }
 
     public static MeuContainerEscolhido lerAlunosDoArquivoXml( String filename ) throws Exception {
+
     }
 
     public static void atualizarResumo( String filename, float mediaGeral, float notaMinima, float notaMaxima, int quantidadeDeAlunos ) throws Exception {
